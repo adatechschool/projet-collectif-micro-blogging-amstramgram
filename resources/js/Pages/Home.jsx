794c803e9eba7e70630329d7f4270e7f1c374702
Djeneba Soumare <djenebasoum98@icloud.com>
2023-08-07 12:11:04 +0200

    add truncate the content and reduce text size on home page

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -9,6 +9,15 @@ export default function Home({ auth, laravelVersion, phpVersion }) {
     const [posts, setPosts] = useState([]);
     const [page, setPage] = useState(1);
     const [isloading, setIsloaDing] = useState(true);
+    const [expandedPosts, setExpandedPosts] = useState({});
+
+    // Gestion de l'affichage de plus de contenu des posts
+    const toggleExpanded = (postId) => {
+        setExpandedPosts({
+            ...expandedPosts,
+            [postId]: !expandedPosts[postId],
+        });
+    };
 
     // Faire un appel à l'API à cet endpoint pour récupérer tous les posts
     useEffect(() => {
@@ -22,6 +31,13 @@ export default function Home({ auth, laravelVersion, phpVersion }) {
             setIsloaDing(false);
         }, 500);
     }, [page]);
+
+    // add limit to the content in the cards
+    function truncateContent(content, limit = 50) {
+        return content.length > limit
+            ? content.substring(0, limit) + "..."
+            : content;
+    }
 
     const handleScroll = () => {
         if (
@@ -38,59 +54,63 @@ export default function Home({ auth, laravelVersion, phpVersion }) {
     }, []);
 
     return (
-<>
-    <Head title="Home" />
-    <AuthenticatedLayout user={auth.user}>
-        <div className="mx-auto text-center space-y-10">
-            <h1 className="text-4xl m-5">Home</h1>
-            <div className="grid md:grid-cols-2 gap-8">
-                {posts.map((post) => (
-                    <div
-                        key={post.id}
-                        className="w-3/4 space-y-6 text-xl bg-purple-100 rounded-md m-auto pb-5"
-                    >
-                        {/* Author */}
-                        <p className="text-sm text-left p-2">
-                            Posted by {post.user.name}
-                        </p>
+        <>
+            <Head title="Home" />
+            <AuthenticatedLayout user={auth.user}>
+                <div className="mx-auto text-center space-y-10">
+                    <h1 className="text-4xl m-5">Home</h1>
+                    <div className="grid md:grid-cols-2 gap-8">
+                        {posts.map((post) => (
+                            <div
+                                key={post.id}
+                                className="w-3/4 space-y-6 text-xl bg-purple-100 rounded-md m-auto pb-5"
+                            >
+                                {/* Author */}
+                                <p className="text-sm text-left p-2">
+                                    Posted by {post.user.name}
+                                </p>
 
-                        {/* Image */}
-                        <div className="flex justify-center">
-                            <img
-                                src="https://picsum.photos/200/300"
-                                alt={post.title}
-                                className="w-full h-1/4 object-cover"
-                            />
-                        </div>
+                                {/* Image */}
+                                <div className="flex justify-center">
+                                    <img
+                                        src="https://picsum.photos/200/300"
+                                        alt={post.title}
+                                        className="w-full h-1/4 object-cover"
+                                    />
+                                </div>
 
-                        {/* Reaction Icons */}
-                        <div className="ReactionIcon flex items-start space-x-4 pl-2">
-                            <div className="hover:text-red-400">
-                                <FontAwesomeIcon icon={faHeart} /> {post.likes}
+                                {/* Reaction Icons */}
+                                <div className="ReactionIcon flex items-start space-x-4 pl-2">
+                                    <div className="hover:text-red-400">
+                                        <FontAwesomeIcon icon={faHeart} />{" "}
+                                        {post.likes}
+                                    </div>
+
+                                    <div className="hover:text-slate-400">
+                                        <FontAwesomeIcon icon={faComment} />{" "}
+                                        {post.comments}
+                                    </div>
+                                </div>
+
+                                {/* Title and content of the post */}
+                                <h2 className="text-left pl-2 mt-4 mb-2 text-lg">
+                                    {post.title}
+                                </h2>
+                                <p
+                                    className="mb-4 overflow-ellipsis pl-2 cursor-pointer text-left text-sm"
+                                    onClick={() => toggleExpanded(post.id)}
+                                >
+                                    {expandedPosts[post.id]
+                                        ? post.content
+                                        : truncateContent(post.content)}
+                                </p>
                             </div>
-
-                            <div className="hover:text-slate-400">
-                                <FontAwesomeIcon icon={faComment} />{" "}
-                                {post.comments}
-                            </div>
-                        </div>
-
-                        {/* Title and content of the post */}
-                        <h2 className="text-left pl-2 mt-4 mb-2 ">
-                            {post.title}
-                        </h2>
-                        <p className=" mb-4 overflow-ellipsis pl-2">
-                            {post.content}
-                        </p>
+                        ))}
                     </div>
-                ))}
-            </div>
-        </div>
+                </div>
 
-        {isloading && <BarLoader />}
-    </AuthenticatedLayout>
-</>
-
+                {isloading && <BarLoader />}
+            </AuthenticatedLayout>
+        </>
     );
 }
-
